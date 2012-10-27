dced1581030751f2df661d194dadfa0ee044d65c
Daniel Bachhuber <danielbachhuber@gmail.com>
2012-10-27 17:51:00 -0500

    A basic pattern for parsing direct email replies from Sparrow

--- what-the-email.php
+++ what-the-email.php
@@ -13,12 +13,30 @@ class What_The_Email
 
 	private static $instance;
 
+	private static $email_body_regex = array(
+			// On Saturday, October 27, 2012 at 3:28 PM, Testing wrote:
+			'(On|At|In|Il|Em|Pada|Le|Am){1}\s.+(wrote|writes|scritto|escreveu|menulis|écrit|(schrieb.+)){1}\s?:',
+		);
+
 	public static function instance() {
 
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new What_The_Email;
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Given body text for an email, parse out all of the quoted
+	 * junk, stuff added by the email client, etc.
+	 */
+	public function get_message( $email_body ) {
+		foreach( self::$email_body_regex as $pattern ) {
+			if ( preg_match( '/^((.|\n)*)' . $pattern . '/iU', $email_body, $matches ) ) {
+				return trim( $matches[1] );
+			}
+		}
+		return $email_body;
 	}
 
 }
